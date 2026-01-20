66423b340a7d276864411e0dd1cfb2a1fc7f6c10
arman <feyziarman@gmail.com>
2026-01-20 13:44:45 +0100

    chore: Remove S3 copy commands for `sbom-reports.json` and `cluster-sbom-reports.json`.

--- dashboard/docker-entrypoint.sh
+++ dashboard/docker-entrypoint.sh
@@ -40,8 +40,6 @@ if [ -n "$S3_BUCKET" ] && [ "$DISABLE_S3_SYNC" != "true" ]; then
                 aws s3 cp "s3://${S3_BUCKET}/${S3_PREFIX:-vuln}/$cluster/cluster-compliance-reports.json" "/usr/share/nginx/html/data/${cluster}-cluster-compliance-reports.json" 2>/dev/null || true
                 aws s3 cp "s3://${S3_BUCKET}/${S3_PREFIX:-vuln}/$cluster/cluster-vulnerability-reports.json" "/usr/share/nginx/html/data/${cluster}-cluster-vulnerability-reports.json" 2>/dev/null || true
                 aws s3 cp "s3://${S3_BUCKET}/${S3_PREFIX:-vuln}/$cluster/rbac-assessment-reports.json" "/usr/share/nginx/html/data/${cluster}-rbac-assessment-reports.json" 2>/dev/null || true
-                aws s3 cp "s3://${S3_BUCKET}/${S3_PREFIX:-vuln}/$cluster/sbom-reports.json" "/usr/share/nginx/html/data/${cluster}-sbom-reports.json" 2>/dev/null || true
-                aws s3 cp "s3://${S3_BUCKET}/${S3_PREFIX:-vuln}/$cluster/cluster-sbom-reports.json" "/usr/share/nginx/html/data/${cluster}-cluster-sbom-reports.json" 2>/dev/null || true
                 aws s3 cp "s3://${S3_BUCKET}/${S3_PREFIX:-vuln}/$cluster/cluster-config-audit-reports.json" "/usr/share/nginx/html/data/${cluster}-cluster-config-audit-reports.json" 2>/dev/null || true
             done
             sleep 30
